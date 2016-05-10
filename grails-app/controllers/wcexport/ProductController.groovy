57d171e7612ede7294b3c74271741e34ae17b68c
mvniekerk <mvniekerk@chazaqdev.com>
2016-05-11 00:24:10 +0200

    Add variations to excel

--- grails-app/controllers/wcexport/ProductController.groovy
+++ grails-app/controllers/wcexport/ProductController.groovy
@@ -5,6 +5,7 @@ import grails.rest.*
 import grails.converters.*
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
 import org.apache.poi.ss.usermodel.Cell
+import org.apache.poi.ss.usermodel.Hyperlink
 
 class ProductController {
 
@@ -23,6 +24,7 @@ class ProductController {
                 response.setHeader "Content-disposition", "attachment; filename=${new Date()}.xlsx"
                 response.contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
                 def wb = new HSSFWorkbook()
+                def creationHelper = wb.creationHelper
                 def s = wb.createSheet 'Products'
                 //Header row
                 def hr = s.createRow 0
@@ -31,16 +33,33 @@ class ProductController {
                     def cell = hr.createCell i, Cell.CELL_TYPE_STRING
                     cell.setCellValue entry
                 }
+
+                def extra = 1
                 l.eachWithIndex { Product p, int i ->
-                    def r = s.createRow(i+1)
+                    def r = s.createRow(i+extra)
                     r.createCell 0, Cell.CELL_TYPE_NUMERIC setCellValue p.id
-                    r.createCell 2, Cell.CELL_TYPE_NUMERIC setCellValue p.price
-                    r.createCell 3, Cell.CELL_TYPE_BOOLEAN setCellValue p.inStock
-                    r.createCell 4, Cell.CELL_TYPE_NUMERIC setCellValue p.stock
-                    r.createCell 5, Cell.CELL_TYPE_NUMERIC setCellValue p.minPrice
-                    r.createCell 6, Cell.CELL_TYPE_NUMERIC setCellValue p.maxPrice
-                    r.createCell 7, Cell.CELL_TYPE_STRING setCellValue p.url
-                    r.createCell 8, Cell.CELL_TYPE_STRING setCellValue p.categories.join(',')
+                    r.createCell 1, Cell.CELL_TYPE_STRING setCellValue p.title
+                    r.createCell 3, Cell.CELL_TYPE_NUMERIC setCellValue p.price
+                    r.createCell 4, Cell.CELL_TYPE_BOOLEAN setCellValue p.inStock
+                    r.createCell 5, Cell.CELL_TYPE_NUMERIC setCellValue p.stock
+                    r.createCell 6, Cell.CELL_TYPE_NUMERIC setCellValue p.minPrice
+                    r.createCell 7, Cell.CELL_TYPE_NUMERIC setCellValue p.maxPrice
+                    r.createCell 8, Cell.CELL_TYPE_STRING setCellValue p.url
+                    def link = creationHelper.createHyperlink(Hyperlink.LINK_URL)
+                    link.address = p.url
+                    r.getCell(8).hyperlink = link
+                    r.createCell 9, Cell.CELL_TYPE_STRING setCellValue p.categories.join(',')
+
+                    p.variations.eachWithIndex{ Variation v, int vi ->
+                        def rr = s.createRow(i+extra+vi)
+                        rr.createCell 0, Cell.CELL_TYPE_NUMERIC setCellValue v.id
+                        rr.createCell 2, Cell.CELL_TYPE_STRING setCellValue v.name
+                        rr.createCell 3, Cell.CELL_TYPE_NUMERIC setCellValue v.price
+                        rr.createCell 4, Cell.CELL_TYPE_BOOLEAN setCellValue v.inStock
+                        rr.createCell 5, Cell.CELL_TYPE_NUMERIC setCellValue v.stock
+                        rr.createCell 6, Cell.CELL_TYPE_NUMERIC setCellValue v.salePrice
+                    }
+                    extra += p.variations.size()
                 }
                 wb.write response.outputStream
             }
